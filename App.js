9e54220fd65b71ed6c51cce85482f49e9d49bf14
saifurrehmanahmed <saifurrehmanahmed@gmail.com>
2022-06-11 11:17:39 +0500

    code for test

--- App.js
+++ App.js
@@ -66,7 +66,7 @@ const App: () => Node = () => {
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
         <Header />
-        
+        <Text>Saif</Text>
       </ScrollView>
     </SafeAreaView>
   );
